f4a524443165da565cfe92a7cb16d9757871d94f
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-07-18 18:27:21 -0500

    Revert "Merge branch 'main' of https://github.com/jhoitenga/whatabook"
    This reverts commit 40fbbe6c81fb0d0214411c70823c82d3f2f4e2a8, reversing
    changes made to 647a3dd7005afad29807626535958c682bd05e4e.

--- whatabook/scripts.js
+++ whatabook/scripts.js
@@ -198,34 +198,6 @@ gameThrones = {
   author: 'George R.R. Martin',
 };
 
-inColdBlood = {
-  bookId: '0574592798',
-  title: 'In Cold Blood',
-  genre: 'True Crime',
-  author: 'Truman Capote',
-};
-
-lifeMafia = {
-  bookId: '059647283',
-  title: 'Donnie Brasco: My Undercover Life in the Mafia',
-  genre: 'True Crime',
-  author: 'Joseph D. Pistone',
-};
-
-diaryAnne = {
-  bookId: '148760891',
-  title: 'The Diary of a Young Girl',
-  genre: 'History',
-  author: 'Anne Frank',
-};
-
-iFunny = {
-  bookId: '031452607',
-  title: 'I Funny: A Middle School Story',
-  genre: 'Comedy',
-  author: 'James Patterson',
-};
-
 // Inserting all the documents for books.
 db.books.insertOne(potterPhilos);
 db.books.insertOne(frankenstein);
@@ -244,10 +216,6 @@ db.books.insertOne(kingIt);
 db.books.insertOne(carrie);
 db.books.insertOne(fireBlood);
 db.books.insertOne(gameThrones);
-db.books.insertOne(inColdBlood);
-db.books.insertOne(lifeMafia);
-db.books.insertOne(diaryAnne);
-db.books.insertOne(iFunny);
 
 // Adding the data for the customers
 emma = {
